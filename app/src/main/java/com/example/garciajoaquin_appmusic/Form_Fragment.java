c1697b2c5d72073bacea32bda7499608ac5c0b5c
joaquingb4 <joaquin_alonsogarcia@iesjoandaustria.org>
2021-10-28 19:13:05 +0200

    Login finished+start MenuActivity --I have to build the content of the fragments

--- app/src/main/java/com/example/garciajoaquin_appmusic/Form_Fragment.java
+++ app/src/main/java/com/example/garciajoaquin_appmusic/Form_Fragment.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link Home_Fragment#newInstance} factory method to
+ * Use the {@link Form_Fragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Home_Fragment extends Fragment {
+public class Form_Fragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class Home_Fragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public Home_Fragment() {
+    public Form_Fragment() {
         // Required empty public constructor
     }
 
@@ -34,11 +34,11 @@ public class Home_Fragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment Home_Fragment.
+     * @return A new instance of fragment Form_Fragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static Home_Fragment newInstance(String param1, String param2) {
-        Home_Fragment fragment = new Home_Fragment();
+    public static Form_Fragment newInstance(String param1, String param2) {
+        Form_Fragment fragment = new Form_Fragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +59,6 @@ public class Home_Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return inflater.inflate(R.layout.fragment_form_, container, false);
     }
 }
--- app/src/main/java/com/example/garciajoaquin_appmusic/MenuActivity.java
+++ app/src/main/java/com/example/garciajoaquin_appmusic/MenuActivity.java
@@ -17,12 +17,18 @@ public class MenuActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_menu);
         BottomNavigationView bottomNav = findViewById(R.id.main_menu);
-
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_contaner, new Home_Fragment()).commit();
         bottomNav.setOnItemSelectedListener(item -> {
             Fragment selectedFragment = null;
             switch (item.getItemId()){
                 case R.id.nav_home:
                     selectedFragment = new Home_Fragment();
+                    break;
+                case R.id.nav_list:
+                    selectedFragment = new List_Fragment();
+                    break;
+                case R.id.nav_form:
+                    selectedFragment = new Form_Fragment();
                     break;
             }
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_contaner, selectedFragment).commit();
