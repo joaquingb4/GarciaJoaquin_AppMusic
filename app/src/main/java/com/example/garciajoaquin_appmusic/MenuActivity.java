938535c3cb02d28ba44b6b1ff36f1bfc435dbf52
joaquingb4 <joaquin_alonsogarcia@iesjoandaustria.org>
2021-10-28 17:32:28 +0200

    Login finished+start MenuActivity --error menu fragment

--- app/src/main/java/com/example/garciajoaquin_appmusic/MenuActivity.java
+++ app/src/main/java/com/example/garciajoaquin_appmusic/MenuActivity.java
@@ -1,14 +1,34 @@
 package com.example.garciajoaquin_appmusic;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MenuActivity extends AppCompatActivity {
+    //Attributes
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_menu);
+        BottomNavigationView bottomNav = findViewById(R.id.main_menu);
+
+        bottomNav.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+            switch (item.getItemId()){
+                case R.id.nav_home:
+                    selectedFragment = new Home_Fragment();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_contaner, selectedFragment).commit();
+            return true;
+        });
+
+
     }
 }
